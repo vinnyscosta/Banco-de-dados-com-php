65b8521b32ee9d16e9500418671036b8e30724a0
VINNYSCOSTA <44821757+vinnyscosta@users.noreply.github.com>
2021-06-15 23:48:26 -0300

    Create select.php
    aqui vemos como funciona a consulta SELECT em PHP

---
+++ select.php
@@ -0,0 +1,26 @@
+<?php
+
+//criação da query com SELECT
+$query_select = "SELECT * FROM table_name";
+
+//Resultado do select é adicionado a uma váriavel, aqui utilizamos a variavel de conexão com o banco e a que possui a query
+$resultado_select = mysqli_query($mysqli,$query_select);
+
+//Verificamos se houve resultados
+//se não, imprimimos a mensagem com o erro na tela
+if (!$resultado_select) {
+	$message  = 'Invalid query: ' . mysql_error() . "\n";
+	$message .= 'Whole query: ' . $query;
+	echo "erro";
+	die($message);
+}else{
+    //se sim, transformamos o resultado em um array para que possamos usá-lo com um while em alguma variavel
+	while ($row = mysqli_fetch_array($resultado_select)) {
+		//aqui usamos a variavel escolhida para nos referenciarmos ao item resultado do SELECT
+        echo $row[1];
+	}
+    //liberamos os resultados obtidos
+    mysqli_free_result($resultado_select);
+}
+
+?>
